fa0b240caa22a70f369e57cf8e24cb12b26389d4
Kotaro Kanke <kanke.kooooo01@gmail.com>
2026-01-25 00:50:06 +0900

    fix: path error

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://k-kanke.github.io/mypage/',
+	base: '/mypage',
 	output: 'static',
 });
